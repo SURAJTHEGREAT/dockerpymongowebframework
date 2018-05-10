eb897aef856db884e03c4cfb40cf4b4c998e8f5e
SURAJTHEGREAT <suraj.gate2013@gmail.com>
2018-05-10 14:37:30 +0530

    Update entrypoint-1ppc.sh

--- blog/bin/entrypoint-1ppc.sh
+++ blog/bin/entrypoint-1ppc.sh
@@ -2,6 +2,12 @@
 
 case "$WEB_SERVICE" in
   "bottle" )
+    pytest /web/$git_repo/tests > test_output.log
+    a=`grep -rnw 'raise AssertionError()' test_output.log`
+    if [ -z ${a} ]; then
     exec python /web/$git_repo/blog.py
+    else
+    echo "Some mandatory config is missed please check"
+    fi
     ;;
 esac
