97eb2a1752e5443427eceb1cfdc4ee5c472fd6d7
SURAJTHEGREAT <suraj.gate2013@gmail.com>
2018-05-10 15:11:32 +0530

    Update entrypoint-1ppc.sh

--- blog/bin/entrypoint-1ppc.sh
+++ blog/bin/entrypoint-1ppc.sh
@@ -3,7 +3,7 @@
 case "$WEB_SERVICE" in
   "bottle" )
     pytest /web/$git_repo/tests > test_output.log
-    a=`grep -rnw 'raise AssertionError()' test_output.log`
+    a=`grep -rnw 'raise AssertionError()' /web/test_output.log`
     if [ -z $a ]; then
     exec python /web/$git_repo/blog.py
     else
