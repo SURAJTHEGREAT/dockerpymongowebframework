06e8678e1bb48a2c4da8cb5686b6ad0ffe7040fd
SURAJTHEGREAT <suraj.gate2013@gmail.com>
2018-05-16 12:34:41 +0530

    Update entrypoint-1ppc.sh

--- blog/bin/entrypoint-1ppc.sh
+++ blog/bin/entrypoint-1ppc.sh
@@ -2,11 +2,13 @@
 
 case "$WEB_SERVICE" in
   "bottle" )
+    # RUN PYTEST MODULE  
     pytest /web/$git_repo/tests > test_output.log
     a=`grep 'FAILURES' /web/test_output.log`
     if [ -z $a ]; then
     exec python /web/$git_repo/blog.py
     else
+    # IF TEST FAILS - DO NOT EXECUTE THE APP
     echo "Test failed please check if all environment variables are filled"
     fi
     ;;
